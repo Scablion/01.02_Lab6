9bedc52be5db007fca2674617d45235626a60b66
User <User@PC-202-07>
2023-05-30 12:36:56 +0300

    6lab 01.02

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Class1
+    {
+        public static int minNum(int [,] mas)
+        {
+            int min = 0, K = 1;
+            for (int i = 11; i >= 0; i--)
+            {
+                for (int k = 11 - i; k <= i; k++)
+                {
+                    if (K == 1)
+                    {
+                        min = mas[i, k];
+                        K++;
+                    }
+                    else
+                    {
+                        if (min > mas[i, k])
+                        {
+                            min = mas[i, k];
+                        }
+                    }
+                }
+            }
+            return min;
+        } 
+
+        public static int[] ArrayW(int[,] mas)
+        {
+            
+            int g = 0,c = 0;
+            for (int i = 11; i >= 0; i--)
+            {
+                for (int k = 11 - i; k <= i; k++)
+                {
+                    if (mas[i, k] < 7)
+                    {
+                        g++;
+                    }
+                }
+            }
+            int[] W = new int [g];
+            for (int i = 11; i >= 0; i--)
+            {
+                for (int k = 11 - i; k <= i; k++)
+                {
+                    if (mas[i, k] < 7)
+                    {
+                        W[c] += mas[i, k];
+                        c++;
+                    }
+                }
+            }
+            return W;
+        }
+
+        public static int[] ArrayN(int[,] mas)
+        {
+
+            int g = 0, c = 0;
+            int[] W = new int[6];
+            for (int i = 11; i >= 6; i--)
+            {
+                for (int k = 11 - i; k <= i; k++)
+                {
+                    if (mas[i, k] < 0)
+                    {
+                        g++;
+                    }
+                }
+                W[c] = g;
+                g = 0;
+                c++;
+            }
+            return W;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Class1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[,] mas = new int[12, 12];
+            for (int i = 0; i < 12; i++)
+            {
+                for (int k = 0; k < 12; k++)
+                {
+
+                    mas[i, k] =/* rnd.Next(-100, 100)*/ 0;
+                }
+            }
+
+            for (int i = 11; i >= 0; i--)
+            {
+                for (int k = 11 - i; k <= i; k++)
+                {
+                    mas[i, k] = 1;
+                }
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                for (int k = 0; k < 12; k++)
+                {
+                    if (mas[i, k] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(mas[i, k] + "\t");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write(mas[i, k] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                for (int k = 0; k < 12; k++)
+                {
+
+                    mas[i, k] = rnd.Next(-100, 100);
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < 12; i++)
+            {
+                for (int k = 0; k < 12; k++)
+                {
+                    Console.Write(mas[i, k] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            //Zadanie1
+            Console.WriteLine("Minimal value = " + minNum(mas));
+            Console.WriteLine();
+
+            //Zadanie2
+            int[] K = ArrayW(mas);
+            for (int i = 0; i < K.Length; i++)
+            {
+                Console.Write($"{K[i]}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Zadanie 3
+            Console.Write("Кол-во отриц. чисел в строках: ");
+            int[] N = ArrayN(mas);
+            for (int i = 0; i < N.Length; i++)
+            {
+                Console.Write($"{N[i]}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Zadanie 4
+
+
+        }
+    }
+}
